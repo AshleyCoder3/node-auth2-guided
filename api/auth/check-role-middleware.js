6c5cb100b3db88fcea1296f7f25392582ee0087a
Ashley Burns <Ash_burn999@yahoo.com>
2021-09-14 13:44:23 -0400

    ..

--- api/auth/check-role-middleware.js
+++ api/auth/check-role-middleware.js
@@ -1,14 +1,14 @@
 
-module.exports = function (req, res, next) { //call back
-  const { decodedJwt } = req;
-
-  if (decodedJwt.role === 'admin') {
-    next();
-  } else {
-    next({
-      status: 403,
-      message: 'You have no power over me!'
-    });
-  }
-
+module.exports = function (role) {
+  return function (req, res, next) { //call back
+    const { decodedJwt } = req;
+    if (decodedJwt.role === role) {
+      next();
+    } else {
+      next({
+        status: 403,
+        message: 'You have no power over me!'
+      });
+    }
+  };
 };
